873297734bd4e0d9e1f999efaee3d1a16c756371
Selvakumar <selvakumard274@gmail.com>
2024-05-03 21:10:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useReducer } from 'react'
+const initialTodos=[{id:1,name:"selva",Complete:true},{id:2,name:"kumar",Complete:false}]
+
+const reducer=(state,action)=>{
+    switch(action.type){
+      case "complete":
+        return state.map((todo)=>{
+           if(todo.id===action.id){
+          return {...todo,Complete:!todo.Complete};
+        }else{
+          return todo;
+        }
+        });
+      default:
+        return state;
+    }
+
+};
+
+const App = () => {
+  const[todos,dispatch]=useReducer(reducer,initialTodos);
+
+  function handlechange(todo){
+    dispatch({type:"complete",id:todo.id});
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {todos.map((todo)=>(
+       <div>
+       <h1>{todo.name}</h1>
+        <input type='checkbox' checked={todo.Complete} onChange={()=>handlechange(todo)}/>
+       </div>
+      ))}
+     
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
